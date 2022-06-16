0808e41e41d6d308a0ad34aede1eb326344b84cb
palakdevgan <m7.palak@gmail.com>
2022-06-16 10:14:54 -0400

    added code for replies functionality

--- models/Pizza.js
+++ models/Pizza.js
@@ -31,11 +31,12 @@ const PizzaSchema = new Schema({
       getters: true
     },
     id: false
-  });
+  }
+);
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function () {
-  return this.comments.length;
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 
 // create the Pizza model using the PizzaSchema
